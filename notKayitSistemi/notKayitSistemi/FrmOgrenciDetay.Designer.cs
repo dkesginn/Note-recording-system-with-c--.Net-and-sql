c91364b233e127cabbfc93165e7960116ec5af6e
Davut Kesgin <98715803+dkesginn@users.noreply.github.com>
2023-03-11 18:02:45 +0300

    Not Kayıt Sistemi
    C# ile yaptığım ilk projem.

---
+++ notKayitSistemi/notKayitSistemi/FrmOgrenciDetay.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace notKayitSistemi
+{
+    partial class FrmOgrenciDetay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblAdSoyad = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblSinav3 = new System.Windows.Forms.Label();
+            this.lblSinav2 = new System.Windows.Forms.Label();
+            this.lblSinav1 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblNumara = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.lblOrtalama = new System.Windows.Forms.Label();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad Soyad:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblAdSoyad
+            // 
+            this.lblAdSoyad.AutoSize = true;
+            this.lblAdSoyad.Location = new System.Drawing.Point(134, 45);
+            this.lblAdSoyad.Name = "lblAdSoyad";
+            this.lblAdSoyad.Size = new System.Drawing.Size(64, 24);
+            this.lblAdSoyad.TabIndex = 1;
+            this.lblAdSoyad.Text = "NULL";
+            this.lblAdSoyad.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Sınav 1:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(51, 165);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Sınav 2:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(51, 210);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 24);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Sınav 3:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // lblSinav3
+            // 
+            this.lblSinav3.AutoSize = true;
+            this.lblSinav3.Location = new System.Drawing.Point(138, 210);
+            this.lblSinav3.Name = "lblSinav3";
+            this.lblSinav3.Size = new System.Drawing.Size(34, 24);
+            this.lblSinav3.TabIndex = 6;
+            this.lblSinav3.Text = "00";
+            // 
+            // lblSinav2
+            // 
+            this.lblSinav2.AutoSize = true;
+            this.lblSinav2.Location = new System.Drawing.Point(138, 165);
+            this.lblSinav2.Name = "lblSinav2";
+            this.lblSinav2.Size = new System.Drawing.Size(34, 24);
+            this.lblSinav2.TabIndex = 7;
+            this.lblSinav2.Text = "00";
+            // 
+            // lblSinav1
+            // 
+            this.lblSinav1.AutoSize = true;
+            this.lblSinav1.Location = new System.Drawing.Point(138, 119);
+            this.lblSinav1.Name = "lblSinav1";
+            this.lblSinav1.Size = new System.Drawing.Size(34, 24);
+            this.lblSinav1.TabIndex = 8;
+            this.lblSinav1.Text = "00";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(41, 78);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(89, 24);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Numara:";
+            // 
+            // lblNumara
+            // 
+            this.lblNumara.AutoSize = true;
+            this.lblNumara.Location = new System.Drawing.Point(138, 78);
+            this.lblNumara.Name = "lblNumara";
+            this.lblNumara.Size = new System.Drawing.Size(58, 24);
+            this.lblNumara.TabIndex = 10;
+            this.lblNumara.Text = "0000";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(32, 258);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(100, 24);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "Ortalama:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(47, 299);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(81, 24);
+            this.label12.TabIndex = 12;
+            this.label12.Text = "Durum:";
+            // 
+            // lblOrtalama
+            // 
+            this.lblOrtalama.AutoSize = true;
+            this.lblOrtalama.Location = new System.Drawing.Point(138, 258);
+            this.lblOrtalama.Name = "lblOrtalama";
+            this.lblOrtalama.Size = new System.Drawing.Size(34, 24);
+            this.lblOrtalama.TabIndex = 13;
+            this.lblOrtalama.Text = "00";
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.Location = new System.Drawing.Point(134, 299);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(64, 24);
+            this.lblDurum.TabIndex = 14;
+            this.lblDurum.Text = "NULL";
+            // 
+            // FrmOgrenciDetay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Moccasin;
+            this.ClientSize = new System.Drawing.Size(403, 403);
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.lblOrtalama);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.lblNumara);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.lblSinav1);
+            this.Controls.Add(this.lblSinav2);
+            this.Controls.Add(this.lblSinav3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblAdSoyad);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FrmOgrenciDetay";
+            this.Text = "Öğrenci Not Kayıt Sistemi";
+            this.Load += new System.EventHandler(this.FrmOgrenciDetay_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblAdSoyad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblSinav3;
+        private System.Windows.Forms.Label lblSinav2;
+        private System.Windows.Forms.Label lblSinav1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblNumara;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label lblOrtalama;
+        private System.Windows.Forms.Label lblDurum;
+    }
+}
